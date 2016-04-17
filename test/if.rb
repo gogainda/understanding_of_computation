a14dae614c8588550e3382b744fe68df240f43e2
gogainda <gogainda@yandex.ru>
2016-04-18 01:21:24 +0400

    Add "if" statement

---
+++ test/if.rb
@@ -0,0 +1,24 @@
+Dir[File.join(File.dirname(__FILE__), '..', 'simple_ast', '*.rb')].each {|file| require file }
+require "minitest/autorun"
+
+class TestIf < Minitest::Test
+  def setup
+    @if = If.new(
+      Variable.new(:x),
+      Number.new(1),
+      Number.new(2)
+    )
+  end
+
+  def test_consequence
+    reduced_if_statement, env = @if.reduce({ x: Boolean.new(true) })
+
+    assert_equal [Number.new(1), env], reduced_if_statement.reduce(env)
+  end
+
+  def test_alternative
+    reduced_if_statement, env = @if.reduce({ x: Boolean.new(false) })
+
+    assert_equal [Number.new(2), env], reduced_if_statement.reduce(env)
+  end
+end
